9ab4598040c2492120fcd7338fb39757f0852a2b
鸡爱上黄鼠狼 <1955098530@qq.com>
2023-04-06 22:20:38 +0800

    利用宏重构中断处理代码

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -8,7 +8,7 @@
 
 static gate_desc_t idt_table[IDT_TABLE_NR];
 
-static do_default_handler(exception_frame_t *frame, const char *message) {
+static void do_default_handler(exception_frame_t *frame, const char *message) {
     for (;;) {}
 }
 
@@ -16,13 +16,28 @@ void do_handler_unknown(exception_frame_t *frame) {
     do_default_handler(frame, "unknown exception");
 }
 
-void exception_handler_unknown(void);
+void do_handler_divider(exception_frame_t *frame) {
+    do_default_handler(frame, "Divder exception");
+}
 
 void irq_init(void) {	
 	for (uint32_t i = 0; i < IDT_TABLE_NR; i++) {
     	gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t) exception_handler_unknown,
                   GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
 	}
+
+    irq_install(IRQ0_DE, (irq_handler_t)exception_handler_divider);
+
 	lidt((uint32_t)idt_table, sizeof(idt_table));
 }
 
+// 安装中断处理程序
+int irq_install(int irq_num, irq_handler_t handler) {
+    if (irq_num > IDT_TABLE_NR) {
+        return -1;
+    }
+
+    gate_desc_set(idt_table + irq_num, KERNEL_SELECTOR_CS, (uint32_t)handler,
+                GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
+    return 0;
+}
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -1,12 +1,22 @@
 #ifndef IRQ_H
 #define IRQ_H
 
+// 中断号
+#define IRQ0_DE     0
+
 typedef struct _exception_frame_t {
     uint32_t gs, fs, es, ds;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecs, eax;
+    uint32_t num;
+    uint32_t error_code;
     uint32_t eip, cs, eflags;
 } exception_frame_t;
 
+typedef void (*irq_handler_t)(void);
+
 void irq_init(void);
+int irq_install(int irq_num, irq_handler_t handler);
+void exception_handler_unknown(void);
+void exception_handler_divider(void);
 
 #endif
