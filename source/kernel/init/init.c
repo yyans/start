2c5f4bb43884bbc60e65eaedb8908dc26f2f2e1c
鸡爱上黄鼠狼 <1955098530@qq.com>
2023-04-23 22:43:35 +0800

    实现链表的头、尾插入

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -34,8 +34,30 @@ void init_task_entry (void) {
 
 void list_test (void) {
     list_t list;
+    list_node_t nodes[5];
+
     list_init(&list);
     log_printf("f: %x, last: %x, count: %d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert : first to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_first(&list, node);
+    }
+
+    log_printf("f: %x, last: %x, count: %d", 
+        list_first(&list), list_last(&list), list_count(&list));
+
+    list_init(&list);
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert : first to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+
+    log_printf("f: %x, last: %x, count: %d", 
+        list_first(&list), list_last(&list), list_count(&list));
+
 }
 
 void init_main() {
